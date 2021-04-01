4f7e3bb77a806bab7bbbe78644d92ebd980267ee
dbetka <dominik.betka@gmail.com>
2021-04-01 19:23:56 +0200

    Fix push-to-whatusea.sh script

--- bin/push-to-whatusea.sh
+++ bin/push-to-whatusea.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 #dry_run=--dry-run
 dry_run=""
-exclude="--exclude={'/.git/', '/.idea/'}"
 vagrant_base=/home/vagrant/sites/dev.whatusea.com/app/webroot/html-api
 local_dist=../
 vagrant_dist=${vagrant_base}/js/app/vendors/material-icons/
 
-rsync -ave "ssh -p 2222" "$dry_run" "$exclude" --delete $local_dist vagrant@localhost:$vagrant_dist
+rsync -ave "ssh -p 2222" $dry_run \
+--exclude='node_modules' \
+--exclude='.git' \
+--exclude='.idea' \
+--delete $local_dist vagrant@localhost:$vagrant_dist
