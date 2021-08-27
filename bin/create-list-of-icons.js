e9539f2ef0d42aafb6bc85066031b701e3ba38f1
dbetka <dominik.betka@gmail.com>
2021-08-27 13:10:23 +0200

    #4 Make lint fix for scripts

--- bin/create-list-of-icons.js
+++ bin/create-list-of-icons.js
@@ -1,41 +1,41 @@
-const fs = require('fs');
+const fs = require('fs')
 const {
   generateIconsDictionary,
   generateTypesDictionary,
   downloadMetadataIcons,
   writeTextIntoFile,
-} = require('../lib/icon-list-generator');
-const logs = require('../lib/logs');
+} = require('../lib/icon-list-generator')
+const logs = require('../lib/logs')
 
-const iconListSource = 'https://fonts.google.com/metadata/icons';
-const jscache = './src/__jscache__/';
-const iconListDestination = jscache + 'icons-names.ts';
-const iconTypesDestination = jscache + 'icons-types.ts';
+const iconListSource = 'https://fonts.google.com/metadata/icons'
+const jscache = './src/__jscache__/'
+const iconListDestination = jscache + 'icons-names.ts'
+const iconTypesDestination = jscache + 'icons-types.ts'
 
 if (!fs.existsSync(jscache)){
-  fs.mkdirSync(jscache);
+  fs.mkdirSync(jscache)
 }
 
 function createFileWithIcons (data = { icons: [] }) {
-  logs.normTitle('Saving icons names and types...');
+  logs.normTitle('Saving icons names and types...')
   return generateIconsDictionary(data.icons)
     .then(dictionary => writeTextIntoFile(iconListDestination, dictionary))
     .then(() => {
-      logs.done(iconListDestination);
-      return data;
-    });
+      logs.done(iconListDestination)
+      return data
+    })
 }
 function createFileWithIconsTypes (data = { families: [] }) {
   return generateTypesDictionary(data.families)
     .then(dictionary => writeTextIntoFile(iconTypesDestination, dictionary))
     .then(() => {
-      logs.done(iconTypesDestination);
-      return data;
-    });
+      logs.done(iconTypesDestination)
+      return data
+    })
 }
 
 downloadMetadataIcons(iconListSource)
   .then(createFileWithIcons)
   .then(createFileWithIconsTypes)
   .then(() => logs.allDone())
-  .catch(console.error);
+  .catch(console.error)
--- bin/download-css-and-fonts.js
+++ bin/download-css-and-fonts.js
@@ -1,118 +1,118 @@
-const fs = require('fs');
-const download = require('download');
+const fs = require('fs')
+const download = require('download')
 const {
   downloadMetadataIcons,
-} = require('../lib/icon-list-generator');
-const logs = require('../lib/logs');
+} = require('../lib/icon-list-generator')
+const logs = require('../lib/logs')
 
-const iconsMetadataURL = 'https://fonts.google.com/metadata/icons';
-const CSSFontsDestination = 'src/__csscache__/';
-const fontsDestination = 'demo/public/html-api/fonts/material-icons/';
-const fontsPublicURL = '/html-api/fonts/material-icons/';
-const importSASSFileName = 'icons.sass';
-const importCSSFileName = 'local-icons.css';
-const fontsContent = [];
+const iconsMetadataURL = 'https://fonts.google.com/metadata/icons'
+const CSSFontsDestination = 'src/__csscache__/'
+const fontsDestination = 'demo/public/html-api/fonts/material-icons/'
+const fontsPublicURL = '/html-api/fonts/material-icons/'
+const importSASSFileName = 'icons.sass'
+const importCSSFileName = 'local-icons.css'
+const fontsContent = []
 
 function addFontDisplayToFile (src) {
-  const content = fs.readFileSync(src, 'utf-8');
-  const newContent = content.replace(/@font-face {\n/gm, '@font-face {\n  font-display: block;\n');
-  fs.writeFileSync(src, newContent, 'utf-8');
-  logs.norm('- add `font-display: block`', { indent: true });
+  const content = fs.readFileSync(src, 'utf-8')
+  const newContent = content.replace(/@font-face {\n/gm, '@font-face {\n  font-display: block;\n')
+  fs.writeFileSync(src, newContent, 'utf-8')
+  logs.norm('- add `font-display: block`', { indent: true })
 }
 
 function addFontDisplayToText (text) {
-  return text.replace(/@font-face {\n/gm, '@font-face {\n  font-display: block;\n');
+  return text.replace(/@font-face {\n/gm, '@font-face {\n  font-display: block;\n')
 }
 
 function downloadCSSAndFontsRecursive (data, links, iter = 0) {
-  const url = links[iter];
-  const fileName = data.families[iter].replace(/\s/g, '-');
-  const fileNameWithExtension = fileName + '.css';
+  const url = links[iter]
+  const fileName = data.families[iter].replace(/\s/g, '-')
+  const fileNameWithExtension = fileName + '.css'
   return download(url, CSSFontsDestination, { filename: fileNameWithExtension })
     .then(content => {
-      saveURLFromCSS(fileName, addFontDisplayToText(String(content)));
-      logs.done(fileNameWithExtension);
-      addFontDisplayToFile(CSSFontsDestination + fileNameWithExtension);
-      iter++;
-      if (links.length > iter) return downloadCSSAndFontsRecursive(data, links, iter);
-      else return Promise.resolve();
-    });
+      saveURLFromCSS(fileName, addFontDisplayToText(String(content)))
+      logs.done(fileNameWithExtension)
+      addFontDisplayToFile(CSSFontsDestination + fileNameWithExtension)
+      iter++
+      if (links.length > iter) return downloadCSSAndFontsRecursive(data, links, iter)
+      else return Promise.resolve()
+    })
 }
 
 function downloadCSSAndFonts (data = { families: [] }) {
-  logs.normTitle('Downloading CSS files...');
+  logs.normTitle('Downloading CSS files...')
   return new Promise((resolve, reject) => {
     const links = data.families
-      .map(type => 'https://fonts.googleapis.com/css2?family=' + type.replace(/\s/g, '+'));
+      .map(type => 'https://fonts.googleapis.com/css2?family=' + type.replace(/\s/g, '+'))
 
     downloadCSSAndFontsRecursive(data, links)
       .then(() => resolve(data.families))
-      .catch(reject);
-  });
+      .catch(reject)
+  })
 }
 
 function createSASSImportFile (families) {
-  logs.normTitle('Create SASS import file...');
+  logs.normTitle('Create SASS import file...')
   return new Promise((resolve, reject) => {
     const data = families
       .map(fileName => `@import "${CSSFontsDestination}${fileName.replace(/\s/g, '-')}"`)
-      .join('\n');
+      .join('\n')
     fs.writeFile(CSSFontsDestination + importSASSFileName, data, err => {
-      if (err) reject(err);
+      if (err) reject(err)
       else {
-        logs.done(importSASSFileName);
-        resolve();
+        logs.done(importSASSFileName)
+        resolve()
       }
-    });
-  });
+    })
+  })
 }
 
 function saveURLFromCSS (fileName, content) {
-  const findURL = content.match(/url\(\S*\)/g);
-  const clearURL = String(findURL).replace('url(', '').replace(')', '');
-  const extension = '.' + clearURL.split('.').pop();
+  const findURL = content.match(/url\(\S*\)/g)
+  const clearURL = String(findURL).replace('url(', '').replace(')', '')
+  const extension = '.' + clearURL.split('.').pop()
   fontsContent.push({
     content,
     fileName,
     file: fileName + extension,
     url: clearURL,
-  });
+  })
 }
 
 function downloadFontRecursive (fontsContent, iter = 0) {
-  const { url, file } = fontsContent[iter];
+  const { url, file } = fontsContent[iter]
   return download(url, fontsDestination, { filename: file })
     .then(() => {
-      logs.done(file);
-      iter++;
-      if (fontsContent.length > iter) return downloadFontRecursive(fontsContent, iter);
-      else return Promise.resolve();
-    });
+      logs.done(file)
+      iter++
+      if (fontsContent.length > iter) return downloadFontRecursive(fontsContent, iter)
+      else return Promise.resolve()
+    })
 }
 
 function downloadFonts () {
-  logs.normTitle('Downloading fonts files...');
+  logs.normTitle('Downloading fonts files...')
   return new Promise((resolve, reject) => {
     downloadFontRecursive(fontsContent)
       .then(resolve)
-      .catch(reject);
-  });
+      .catch(reject)
+  })
 }
 
 function createCSSImportFile () {
-  logs.normTitle('Create CSS import file...');
+  logs.normTitle('Create CSS import file...')
   return new Promise((resolve, reject) => {
     const data = fontsContent
       .map(({ file, content }) => content.replace(/url\(\S*\)/g, `url("${fontsPublicURL + file}")`))
-      .join('\n');
+      .join('\n')
     fs.writeFile(CSSFontsDestination + importCSSFileName, data, err => {
-      if (err) reject(err);
+      if (err) reject(err)
       else {
-        logs.done(importCSSFileName);
-        resolve();
+        logs.done(importCSSFileName)
+        resolve()
       }
-    });
-  });
+    })
+  })
 }
 
 downloadMetadataIcons(iconsMetadataURL)
@@ -121,4 +121,4 @@ downloadMetadataIcons(iconsMetadataURL)
   .then(downloadFonts)
   .then(createCSSImportFile)
   .then(() => logs.allDone())
-  .catch(console.error);
+  .catch(console.error)
